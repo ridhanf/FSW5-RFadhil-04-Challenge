63d09c30d54436e97b82541c17375c0df7b28137
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2020-12-13 16:17:40 +0700

    refresh done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
 const listSign = ["batu", "gunting", "kertas"];
 var timesRun;
+var player1Sign = document.getElementsByClassName("player1Sign")
+var computerSign = document.getElementsByClassName("computerSign")
+var randomIdxPlayer1Before = 0;
+var randomIdxComputerBefore = 0;
+
+const ubahWarnaLatar = (list, idx) => {
+  list[idx].style.backgroundColor = "gray";
+}
+
+const resetWarnaLatar = (list, idx) => {
+  list[idx].style.backgroundColor =  "#AE876B";
+}
 
 const getSign = () => {  
-  const randomIdxPlayer = Math.floor(Math.random() * 3);
+  const randomIdxPlayer1 = Math.floor(Math.random() * 3);
   const randomIdxComputer = Math.floor(Math.random() * 3);
-  console.log("Player:" + listSign[randomIdxPlayer] + "\t Computer:" + listSign[randomIdxComputer]);
+  // console.log("Player1:" + listSign[randomIdxPlayer1] + "\t Computer:" + listSign[randomIdxComputer]);
+  ubahWarnaLatar(player1Sign, randomIdxPlayer1);
+  ubahWarnaLatar(computerSign, randomIdxComputer);
+  if (timesRun > 0) {
+    resetWarnaLatar(player1Sign, randomIdxPlayer1Before);
+    resetWarnaLatar(computerSign, randomIdxComputerBefore);
+  }
+  randomIdxPlayer1Before = randomIdxPlayer1;
+  randomIdxComputerBefore = randomIdxComputer;
   timesRun += 1;
-  if (timesRun == 20) {
+  if (timesRun == 25) {
     clearInterval(intervalGenerator);
-    outputPlayer = listSign[randomIdxPlayer];
+    outputPlayer1 = listSign[randomIdxPlayer1];
     outputComputer = listSign[randomIdxComputer];
-    console.log("Player:" + outputPlayer + "\t Computer:" + outputComputer);
+    ubahWarnaLatar(player1Sign, randomIdxPlayer1);
+    ubahWarnaLatar(computerSign, randomIdxComputer);
+    // console.log("Player1:" + outputPlayer1 + "\t Computer:" + outputComputer);
+    timesRun = 0;
   }
 }
 
 const refreshButton = () => {
   timesRun = 0;
-  intervalGenerator = setInterval(getSign, 100);
+  intervalGenerator = setInterval(getSign, 50);
 }
